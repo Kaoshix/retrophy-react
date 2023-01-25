dbbecbf938757e7f3e53356b67a9d2ea4efa9e2e
Sebastien <sebastien.olha@gmail.com>
2023-01-25 21:01:05 +0100

    Ajout registration

--- src/pages/AdminGames.js
+++ src/pages/AdminGames.js
@@ -15,19 +15,20 @@ export default function AdminGames() {
     return (
         <>
             {games ?
-                <div>
-                    <h1 className="text-4xl">Games</h1>
-                    <table>
-
+                <div className="flex mt-5">
+                    <nav className="min-h-screen bg-red-500 p-5">
+                        <h1 className="text-4xl">Games</h1>
+                    </nav>
+                    <table className="max-w-7xl bg-green-500 p-5">
                         <thead>
-                            <tr>
+                            <tr className="flex justify-between">
                                 <th>Game title</th>
                                 <th>Game cover</th>
                                 <th>Actions</th>
                             </tr>
 
                             {games.map(game => (
-                                <tr key={game.id}>
+                                <tr key={game.id} className="flex justify-between">
                                     <td>{game.title}</td>
                                     <td><img src={game.imagePath} alt={game.slug} /></td>
                                     <td>
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,32 +1,88 @@
+import { useState } from 'react';
 import './LoginRegisterPage.css';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
 export const RegisterPage = () => {
+
+    const [nickName, setNickName] = useState(null);
+    const [username, setUsername] = useState(null);
+    const [password, setPassword] = useState(null);
+    const [confirmPassword, setConfirmPassword] = useState(null);
+
+    const history = useHistory();
+
+    const datas = {
+        nickName: nickName,
+        email: username,
+        password: password
+    }
+
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    async function handleRegister(event) {
+        event.preventDefault();
+
+        if (password !== confirmPassword) {
+            return (console.log("Mot de passe incorrect"));
+        } else {
+            await axios.post('http://127.0.0.1:8000/api/registration', datas, config)
+                .then(response => console.log(response.data))
+                .catch(error => console.log(error))
+            history.push('/login')
+        }
+    }
+
     return (
         <div>
-            <form className="login-register-formular">
-            <h1>Register</h1>
+            <form className="login-register-formular" onSubmit={handleRegister}>
+                <h1>Register</h1>
                 <div className='input-group'>
-                    <label>Username</label>
-                    <input type='text' />
+                    <label htmlFor='nickName'>Nickname</label>
+                    <input
+                        type='text'
+                        id='nickName'
+                        value={nickName}
+                        onChange={(e) => setNickName(e.target.value)}
+                    />
                 </div>
 
                 <div className='input-group'>
-                    <label>Email</label>
-                    <input type='text' />
+                    <label htmlFor='username'>Email</label>
+                    <input
+                        type='username'
+                        id='username'
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
                 </div>
 
                 <div className='input-group'>
                     <label>Password</label>
-                    <input type='password' />
+                    <input
+                        type='password'
+                        id='password'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
                 </div>
 
                 <div className='input-group'>
                     <label>Confirm password</label>
-                    <input type='password' />
+                    <input
+                        type='password'
+                        id='confirmPassword'
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                    />
                 </div>
 
                 <div className='register'>
-                    <button className='register-button'><a href='/#'>Register</a></button>
+                    <button className='register-button' type='submit'>Register</button>
                 </div>
             </form>
         </div>
