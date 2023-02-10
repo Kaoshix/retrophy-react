3d0ad299faecb640b527daa44345ed94929d2c0d
Sebastien <sebastien.olha@gmail.com>
2023-02-10 16:15:53 +0100

    retrait assets front + html mobile games page

--- src/pages/GamesPage.js
+++ src/pages/GamesPage.js
@@ -18,8 +18,8 @@ function GamesPage() {
     fetchData();
   }, []);
   return (
-    <div className="px-4">
-      <ul className="text-3xl text-center py-2 border-b-2 border-gray-600">
+    <div className="px-4 mt-20">
+      <ul className="text-3xl text-center lg:text-left py-2 border-b-2 border-gray-600">
         <li>Games</li>
       </ul>
 
@@ -36,62 +36,61 @@ function GamesPage() {
           </div>
         </div>
 
-        {/* filter */}
-        <div className="bg-slate-100 rounded-lg border-r-2 border-gray-600">
-          <div className="flex justify-between p-3 text-lg font-bold">
-            <h2 className="text-center grow">Search by</h2>
-            <img src={arrowBottom} alt="arrow-bottom" />
+        <div className="lg:flex">
+          {/* filter */}
+          <div className="bg-slate-100 rounded-lg lg:w-[500px] lg:translate-x-[-500px] lg:duration-300 filter">
+            <div className="flex justify-between p-3 text-lg font-bold">
+              <h2 className="text-center grow">Search by</h2>
+              <img src={arrowBottom} alt="arrow-bottom" />
+            </div>
+            <div>
+              <div className="border-b-2 border-black mx-4 pb-1">
+                <h3 className="text-center font-bold pb-2">Notes</h3>
+                <div className="flex justify-around">
+                  <button className="bg-red-300 py-1 px-3 rounded-full">
+                    Ascending
+                  </button>
+                  <button>Descending</button>
+                </div>
+              </div>
+
+              <div className="border-b-2 border-black mx-4 pb-1">
+                <h3 className="text-center font-bold pb-2 pt-3">Most played</h3>
+                <div className="flex justify-around">
+                  <button className="bg-red-300 py-1 px-3 rounded-full">
+                    Ascending
+                  </button>
+                  <button>Descending</button>
+                </div>
+              </div>
+
+              <div className="mx-4 pb-4">
+                <h3 className="text-center font-bold pb-2 pt-3">Difficulty</h3>
+                <div className="flex justify-around">
+                  <button>Ascending</button>
+                  <button className="bg-red-300 py-1 px-3 rounded-full">
+                    Descending
+                  </button>
+                </div>
+              </div>
+              <img src={cross} alt="cross" className="absolute top-0 right-5" />
+            </div>
           </div>
-          <div className="hidden">
-            <div className="border-b-2 border-black mx-4 pb-1">
-              <h3 className="text-center font-bold pb-2">Notes</h3>
-              <div className="flex justify-around">
-                <button className="bg-red-300 py-1 px-3 rounded-full">
-                  Ascending
-                </button>
-                <button>Descending</button>
-              </div>
-            </div>
 
-            <div className="border-b-2 border-black mx-4 pb-1">
-              <h3 className="text-center font-bold pb-2 pt-3">Most played</h3>
-              <div className="flex justify-around">
-                <button className="bg-red-300 py-1 px-3 rounded-full">
-                  Ascending
-                </button>
-                <button>Descending</button>
-              </div>
-            </div>
+          {/* games list */}
+          <div className="lg:flex lg:flex-wrap lg:mx-20 lg:px-20 lg:border-l-2 lg:border-gray-600 lg:relative">
 
-            <div className="mx-4 pb-4">
-              <h3 className="text-center font-bold pb-2 pt-3">Difficulty</h3>
-              <div className="flex justify-around">
-                <button>Ascending</button>
-                <button className="bg-red-300 py-1 px-3 rounded-full">
-                  Descending
-                </button>
+          <img src={options} alt="options" className="hidden lg:block lg:absolute top-5 left-6" onClick={() => {
+            document.querySelector('.filter').classList.toggle('lg:translate-x-[-500px]');
+            
+          }} />
+            {games?.map((game) => (
+                <div className="lg:w-[14vw]">
+              <Game game={game} key={game.id} />
               </div>
-            </div>
-            <img src={cross} alt="cross" className="absolute top-0 right-5" />
+            ))}
           </div>
         </div>
-
-        {/* games list */}
-        {window.innerWidth > 1024 ? (
-          <div className="flex flex-wrap">
-            {games?.map((game) => (
-              <Game game={game} key={game.id} />
-            ))}
-          </div>
-        ) : (
-          <div>
-            {games?.map((game) => (
-              <Game game={game} key={game.id} />
-            ))}
-          </div>
-        )}
-
-        <div>{/* <img src={options} alt="options" className="px-5" /> */}</div>
       </div>
     </div>
   );
