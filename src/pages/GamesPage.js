0dc1537601bc82a060ca0849803993b933146540
Sebastien <sebastien.olha@gmail.com>
2023-03-10 23:28:41 +0100

    refont games page

--- src/pages/GamesPage.js
+++ src/pages/GamesPage.js
@@ -25,7 +25,7 @@ function GamesPage() {
    };
 
    return (
-      <div className="px-4">
+      <div className="max-w-screen-2xl m-auto">
          <ul className="text-3xl text-center py-2 border-b-2 border-gray-600 lg:text-left">
             <li>Games</li>
          </ul>
@@ -47,68 +47,25 @@ function GamesPage() {
 
             <div className="lg:flex">
                {/* filter */}
-               <div className="mb-5 filter lg:w-[30vw] lg:max-h-[50px] lg:mr-20">
-                  <div className="p-3 text-lg bg-slate-100 font-bold relative rounded-lg filter_title">
-                     <h2 className="text-center">Search by</h2>
+               <div className="mb-5 filter lg:mr-6 lg:min-w-[300px]">
+                  <div className="p-3 text-lg bg-slate-100 font-bold relative rounded-lg">
+                     <h2 className="text-center lg:pr-5">Search by</h2>
                      <img
                         src={arrowBottom}
                         alt="arrow-bottom"
                         className="cursor-pointer absolute top-[18px] right-[10px] filter-arrow rotate-[-90deg] duration-200 ease-in-out"
                         onClick={() => {
                            document
-                              .querySelector(".scroll_filter")
-                              .classList.toggle("hidden");
-                           document
-                              .querySelector(".filter_title")
-                              .classList.toggle("rounded-b-none");
-                           document
-                              .querySelector(".filter")
-                              .classList.toggle("lg:max-h-[50px]");
-                           document
                               .querySelector(".filter-arrow")
                               .classList.toggle("rotate-[-90deg]");
                         }}
                      />
                   </div>
-                  <div className="scroll_filter bg-slate-100 rounded-b-lg hidden">
-                     <div className="border-b-2 border-black mx-4 pb-1">
-                        <h3 className="text-center font-bold pb-2">Notes</h3>
-                        <div className="flex justify-around">
-                           <button className="bg-red-300 py-1 px-3 rounded-full">
-                              Ascending
-                           </button>
-                           <button>Descending</button>
-                        </div>
-                     </div>
 
-                     <div className="border-b-2 border-black mx-4 pb-1">
-                        <h3 className="text-center font-bold pb-2 pt-3">
-                           Most played
-                        </h3>
-                        <div className="flex justify-around">
-                           <button className="bg-red-300 py-1 px-3 rounded-full">
-                              Ascending
-                           </button>
-                           <button>Descending</button>
-                        </div>
-                     </div>
-
-                     <div className="mx-4 pb-4">
-                        <h3 className="text-center font-bold pb-2 pt-3">
-                           Difficulty
-                        </h3>
-                        <div className="flex justify-around">
-                           <button>Ascending</button>
-                           <button className="bg-red-300 py-1 px-3 rounded-full">
-                              Descending
-                           </button>
-                        </div>
-                     </div>
-                  </div>
                </div>
 
                {/* games list */}
-               <div className="lg:flex lg:flex-wrap lg:pl-20 lg:border-l-2 lg:border-gray-600 lg:relative">
+               <div className="lg:flex lg:flex-wrap lg:pl-16 lg:border-l-2 lg:border-gray-600 lg:relative">
                   <img
                      src={options}
                      alt="options"
@@ -119,14 +76,29 @@ function GamesPage() {
                            .classList.toggle("lg:hidden");
                      }}
                   />
-                  {games?.filter(game => game.title.toLowerCase().match(searchInput.toLowerCase())).map((game) => (
+                  {games ? games.filter(game => game.title.toLowerCase().match(searchInput.toLowerCase())).map((game) => (
                      <div
-                        className="text-center lg:max-w-[200px] lg:mx-3 mb-5"
+                        className="text-center mb-5 lg:max-w-[200px] lg:mx-3"
                         key={game.id}
                      >
                         <Game game={game} />
                      </div>
                   ))
+                  : <>
+                     <div className="h-[355px] w-[280px] bg-slate-800 m-auto mb-5 animate-pulse lg:w-[200px] lg:h-[254px] lg:mx-3"></div>
+                     <div className="h-[355px] w-[280px] bg-slate-800 m-auto mb-5 animate-pulse lg:w-[200px] lg:h-[254px] lg:mx-3"></div>
+                     <div className="h-[355px] w-[280px] bg-slate-800 m-auto mb-5 animate-pulse lg:w-[200px] lg:h-[254px] lg:mx-3"></div>
+                     <div className="h-[355px] w-[280px] bg-slate-800 m-auto mb-5 animate-pulse lg:w-[200px] lg:h-[254px] lg:mx-3"></div>
+                     <div className="h-[355px] w-[280px] bg-slate-800 m-auto mb-5 animate-pulse lg:w-[200px] lg:h-[254px] lg:mx-3"></div>
+                     <div className="h-[355px] w-[280px] bg-slate-800 m-auto mb-5 animate-pulse lg:w-[200px] lg:h-[254px] lg:mx-3"></div>
+                     <div className="h-[355px] w-[280px] bg-slate-800 m-auto mb-5 animate-pulse lg:w-[200px] lg:h-[254px] lg:mx-3"></div>
+                     <div className="h-[355px] w-[280px] bg-slate-800 m-auto mb-5 animate-pulse lg:w-[200px] lg:h-[254px] lg:mx-3"></div>
+                     <div className="h-[355px] w-[280px] bg-slate-800 m-auto mb-5 animate-pulse lg:w-[200px] lg:h-[254px] lg:mx-3"></div>
+                     <div className="h-[355px] w-[280px] bg-slate-800 m-auto mb-5 animate-pulse lg:w-[200px] lg:h-[254px] lg:mx-3"></div>
+
+                     
+                  </>
+                     
                   }
                </div>
             </div>
